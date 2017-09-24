2909f8144e5a5898237918ca32fe7648659d73b8
JsMayerMin <js.mayer.min@gmail.com>
2017-09-24 21:24:46 +0900

    chapter2 exercise

---
+++ IntroducePython/Chapter2/exercise.py
@@ -0,0 +1,17 @@
+
+#1,2
+seconds_per_hour = 1 * 60 * 60
+print(seconds_per_hour)
+
+#3,4
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+
+#5
+print(seconds_per_day / seconds_per_hour)
+
+#6
+print(seconds_per_day // seconds_per_hour)
+
+
+
